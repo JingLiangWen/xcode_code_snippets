1c702539d4f818f5d777098dfce49857b0aed148
wenjl <wenhappy456@gmail.com>
2018-05-04 14:09:10 +0800

    init

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# xcode_code_snippets" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/JingLiangWen/xcode_code_snippets.git
+git push -u origin master
